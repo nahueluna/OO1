97159781b287504912449543d21065da6fe6a656
nahuel <nahuelnicolasluna@hotmail.com>
2024-09-22 14:03:20 -0300

    Modificación Círculo ejercicio 5 Se eliminó Punto del estado interno del círculo, así como sus métodos asociados

--- ejercicio5/src/main/java/io/github/Circulo.java
+++ ejercicio5/src/main/java/io/github/Circulo.java
@@ -1,16 +1,13 @@
 package io.github;
 
 public class Circulo implements Figura {
-    private Punto punto;
     private double radio;
 
     public Circulo() {
-        this.punto = new Punto();
         this.radio = 0;
     }
 
-    public Circulo(Punto punto, double radio) {
-        this.punto = punto;
+    public Circulo(double radio) {
         this.radio = radio;
     }
 
@@ -28,14 +25,6 @@ public class Circulo implements Figura {
 
     public void setRadio(double radio) {
         this.radio = radio;
-    }
-
-    public Punto getPunto() {
-        return punto;
-    }
-
-    public void setPunto(Punto punto) {
-        this.punto = punto;
     }
 
     public double getPerimetro() {
